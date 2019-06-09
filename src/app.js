a793b836c65e27de0fba74fbdc05cbf3b51d2679
Turuta Aleksandr <turuta51@gmail.com>
2019-06-09 23:40:25 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -12,63 +12,62 @@ if (!token) {
 
 
 const bot = new TelegramBot(token, { polling: true });
+try {
 
-// eslint-disable-next-line no-unused-vars
-bot.onText(/\/course/, msg => {
-    const chatId = msg.chat.id;
-    // const resp = match[1];
-    const resp = 'Выбирете валюту:';
-    bot.sendMessage(chatId, resp, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: 'USD',
-                        callback_data: 'USD',
-                    },
-                    {
-                        text: 'EUR',
-                        callback_data: 'EUR',
-                    },
-                    {
-                        text: 'RUR',
-                        callback_data: 'RUR',
-                    },
-                    {
-                        text: 'BTC',
-                        callback_data: 'BTC',
-                    },
+    // eslint-disable-next-line no-unused-vars
+    bot.onText(/\/course/, msg => {
+        const chatId = msg.chat.id;
+        // const resp = match[1];
+        const resp = 'Выбирете валюту:';
+        bot.sendMessage(chatId, resp, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'USD',
+                            callback_data: 'USD',
+                        },
+                        {
+                            text: 'EUR',
+                            callback_data: 'EUR',
+                        },
+                        {
+                            text: 'RUR',
+                            callback_data: 'RUR',
+                        },
+                        {
+                            text: 'BTC',
+                            callback_data: 'BTC',
+                        },
+                    ],
                 ],
-            ],
-        },
+            },
+        });
     });
-});
 
-bot.on('callback_query', query => {
-    const { id } = query.message.chat;
-    console.log(id);
-    request(
-        'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
-        (error, responss, body) => {
-            const data = JSON.parse(body);
+    bot.on('callback_query', query => {
+        const { id } = query.message.chat;
+        console.log(id);
+        request(
+            'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+            (error, responss, body) => {
+                const data = JSON.parse(body);
+                const result = data.filter(x => x.ccy === query.data)[0];
 
-            console.log(error);
-            console.log(responss);
-
-            const result = data.filter(x => x.ccy === query.data)[0];
-
-            const md = `
+                const md = `
         *${result.ccy} => ${result.base_ccy}*
         Покупка: ${result.buy}
         Продажа: ${result.sale}
         `;
-            console.log(md);
-            bot.sendMessage(id, md, { parse_mode: 'Markdown' });
-        },
-    );
-});
 
+                bot.sendMessage(id, md, { parse_mode: 'Markdown' });
+            },
+        );
+    });
 
+} catch (error) {
+    console.log(error);
+}
 
 // "prettier --write",
 // "eslint --fix --max-warnings 0",
