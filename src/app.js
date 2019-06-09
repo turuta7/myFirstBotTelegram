768a35b45f773e9cc2c275e8cb90b8bb0dba961d
Turuta Aleksandr <turuta51@gmail.com>
2019-06-10 00:22:15 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ try {
     bot.onText(/\/course/, msg => {
         const chatId = msg.chat.id;
         // const resp = match[1];
-        const resp = 'Выбирете валюту:';
+        const resp = 'Выберите валюту:';
         bot.sendMessage(chatId, resp, {
             reply_markup: {
                 inline_keyboard: [
@@ -63,7 +63,7 @@ try {
 } catch (error) {
     console.log(error);
 }
-require('https').createServer().listen(process.env.PORT || 5000).on('request', function (req, res) { Res.end() });
+require('https').createServer().listen(process.env.PORT || 5000).on('request', function (req, res) { res.end() });
 // "prettier --write",
 // "eslint --fix --max-warnings 0",
 // "git add"
