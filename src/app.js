3ff7be95a19a51ec352ab51081d408ee40c6db93
turuta7 <turuta51@gmail.com>
2019-06-10 15:08:48 +0300

    add_weather_bot

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const express = require('express');
+const https = require("https");
+
+const { argv } = require('yargs')
 
 const app = express();
 
@@ -51,31 +54,78 @@ try {
         });
     });
 
+    bot.onText(/\/weather/, msg => {
+        const chatId = msg.chat.id;
+        const resp = 'Погода:';
+        bot.sendMessage(chatId, resp, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'Погода в Черкассах',
+                            callback_data: 'cherkasy',
+                        },
+                        {
+                            text: 'Погода в Киеве',
+                            callback_data: 'kiev',
+                        },
+                    ],
+                ],
+            },
+        });
+    });
+
+
     bot.on('callback_query', query => {
         const { id } = query.message.chat;
-        console.log(id);
-        request(
-            'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
-            (error, responss, body) => {
-                const data = JSON.parse(body);
-                const result = data.filter(x => x.ccy === query.data)[0];
+        if (query.data === 'cherkasy' || query.data === 'kiev') {
+            const { apiKey } = process.env;
+            const city = argv.c || query.data
+            const units = 'metric'
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 
-                const md = `
-        *${result.ccy} => ${result.base_ccy}*
-        Покупка: ${result.buy}
-        Продажа: ${result.sale}
-        `;
+            request(url, (err, response, body) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.log('error:', err)
+                } else {
+                    const weather = JSON.parse(body)
+                    const response1 = `*Сейчас: ${weather.main.temp} градусов в ${weather.name}*
+                      \n Максимальная темперетура: ${weather.main.temp_max} 
+                      \n Минимальная темперетура: ${weather.main.temp_min}
+                      Влажность: ${weather.main.humidity} %
+                      Облачность: ${weather.clouds} %`
+                    bot.sendMessage(id, response1, { parse_mode: 'Markdown' });
+                }
+            })
+        } else {
+            console.log(id);
+            request(
+                'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+                (error, responss, body) => {
+                    const data = JSON.parse(body);
+                    const result = data.filter(x => x.ccy === query.data)[0];
 
-                bot.sendMessage(id, md, { parse_mode: 'Markdown' });
-            },
-        );
+                    const md = `
+            *${result.ccy} => ${result.base_ccy}*
+            Покупка: ${result.buy}
+            Продажа: ${result.sale}
+            `;
+
+                    bot.sendMessage(id, md, { parse_mode: 'Markdown' });
+                },
+            );
+        }
+
+
     });
 
 } catch (error) {
     console.log(error);
 }
 
-const https = require("https");
+
+
 setInterval(function () {
     https.get("https://myfirst-telegrambot.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
@@ -91,3 +141,7 @@ app.listen(process.env.PORT || 3000)
 // "prettier --write",
 // "eslint --fix --max-warnings 0",
 // "git add"
+
+
+
+
